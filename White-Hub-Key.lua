4fe9129452beda0065ed25c716b831c3e626dafd
cecemonr <cecemonr@gmail.com>
2024-11-06 12:28:47 +0200

    Update White-Hub-Key.lua1

--- White-Hub-Key.lua
+++ White-Hub-Key.lua
@@ -1,14 +1,11 @@
 local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
 
--- Ссылка на Pastebin с ключом
 local keyUrl = "https://pastebin.com/raw/uiP40nsG"
---test
--- Функция для загрузки кода WhiteHub
+
 local function loadWhiteHubCode()
    loadstring(game:HttpGet("https://raw.githubusercontent.com/cecemonr/WhiteHub/refs/heads/main/White-Hub.lua"))()
 end
 
--- Создаем окно для ввода ключа
 local Window = OrionLib:MakeWindow({Name = "White Hub - Key", HidePremium = false, SaveConfig = true, ConfigFolder = "WhiteHub"})
 
 local Tab = Window:MakeTab({
@@ -19,7 +16,6 @@ local Tab = Window:MakeTab({
 
 local userInputKey = ""
 
--- Добавляем поле для ввода ключа
 Tab:AddTextbox({
     Name = "Enter Key",
     Default = "",
@@ -29,7 +25,6 @@ Tab:AddTextbox({
     end
 })
 
--- Проверка ключа на сервере
 local function checkKey(callback)
     local response = game:HttpGet(keyUrl)
     if response then
@@ -44,7 +39,6 @@ local function checkKey(callback)
     end
 end
 
--- Добавляем кнопку для проверки ключа
 Tab:AddButton({
     Name = "Check Key",
     Callback = function()
@@ -70,7 +64,6 @@ Tab:AddButton({
     end
 })
 
--- Кнопка для копирования ссылки
 Tab:AddButton({
     Name = "Copy Link",
     Callback = function()
